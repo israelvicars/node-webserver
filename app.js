2b602e10c71aee7e594c0ef15d6449961ce487a7
Israel Vicars <israelvicars@gmail.com>
2015-11-11 00:58:24 -0500

    Cleanup and fixes referencing example repo

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 'use strict';
 
 var express = require('express');
-var app = express();
 var badges = require('./controllers/badges')
 
+var app = express();
+
+ // Have server listen on port 8000
+
+app.listen(8000, function(){
+	console.log('Server is listening on port %d', 8000);
+});
+
+// Parse JSON data from incoming requests
+
 app.use(express.json());
+
+// Accept POST requests and then publish the body of the request
 
 app.post('/', badges.save, badges.send, function(req, res) {
 	res.send('\ndone\n\n');
 });
 
-app.listen(8000);
+// Get the 10 most recent badges
+
+app.get('/badges', badges.get);
--- controllers/badges.js
+++ controllers/badges.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var _ = require('underscore');
 var model = require('../models/badges');
+var _ = require('underscore');
+
+// Save badges
 
 exports.save = function(req, res, next) {
 	var badges = _.clone(req.body);
@@ -11,6 +13,29 @@ exports.save = function(req, res, next) {
 	});
 };
 
+// Trim badges
+
+exports.trim = function(req, res, next) {
+  model.trim();
+  next();
+};
+
+// Send badges to pub/sub socket in model
+
 exports.send = function(req, res, next) {
-	next();
+	var badges = _.clone(req.body);
+	model.send(badges);
+	res.json(200, 'success');
+};
+
+// Get 10 badges from model
+
+exports.get = function(req, res, next) {
+	model.get(function(err, data){
+		res.json(err ? 503 : 200, {
+      error: err ? true : null,
+      errorMessage: err ? err : null,
+      data: data
+    });
+	});
 };
